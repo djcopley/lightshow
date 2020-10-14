be2c7eb5f120a53e5b36e3437486768e57907da5
Noah Levie <71532758+NLevie99@users.noreply.github.com>
2020-10-14 11:34:05 -0700

    Created fade animation

--- lightshow/animations/__init__.py
+++ lightshow/animations/__init__.py
@@ -2,7 +2,7 @@ from importlib import import_module
 
 
 def querry_animations():
-    return "rainbow", "rainbowtheater", "solid", "colorwipe", "strobe", "randomstrobe"
+    return "rainbow", "rainbowtheater", "solid", "colorwipe", "strobe", "randomstrobe", "fade"
 
 
 def get_animation(name: str):
---
+++ lightshow/animations/fade.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from . import Slider, set_strand
+from .animation import Animation, run_decorator
+
+
+class Fade(Animation):
+    def __init__(self, strip, brightness=255, transition=50):
+        super().__init__(strip, brightness)
+        self._transition = Slider("Transition", transition, (1, 100))
+
+    @property
+    def transition(self):
+        return self._transition.value
+
+    @transition.setter
+    def transition(self, value):
+        self._transition.value = value
+
+    @run_decorator
+    async def run(self):
+        while True:
+            for i in range(256):
+                set_strand(self.strip, self.color_wheel(i))
+            await asyncio.sleep(self.transition)
+
+    def get_settings(self):
+        settings = [
+            self._transition
+        ]
+        return super().get_settings() + settings

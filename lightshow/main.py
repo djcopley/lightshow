48543cfceb17497ec20cc057c3ba01cfc9ef2d99
Daniel Copley <djcopley@users.noreply.github.com>
2020-10-24 14:24:57 -0700

    Added permission elevating

--- lightshow/main.py
+++ lightshow/main.py
@@ -1,8 +1,14 @@
+import os
+import elevate
+
 from .views import *
 from .app import app, socketio
 
 
 def main():
+    # Elevate to root permissions for GPIO access
+    if os.getuid() != 0:
+        elevate.elevate()
     socketio.run(app)
 
 

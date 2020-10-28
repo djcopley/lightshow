f66e5123ae6928dd704bcf227483c65215c3a7d4
Noah Levie <71532758+NLevie99@users.noreply.github.com>
2020-10-27 18:21:20 -0700

    Added speed input for Breathe animation
    Signed-off-by: Noah Levie <71532758+NLevie99@users.noreply.github.com>

--- lightshow/animations/breathe.py
+++ lightshow/animations/breathe.py
@@ -17,16 +17,16 @@ class Breathe(Animation):
         """
 
         super().__init__(strip, brightness)
-        self._delay = Slider("Delay", freq, (1, 50))
+        self._speed = Slider("Speed", freq, (1, 50))
         self._color = Color("Color", color)
 
     @property
-    def delay(self):
-        return self._delay.value
+    def speed(self):
+        return self._speed.value
 
-    @delay.setter
-    def delay(self, value):
-        self._delay.value = value
+    @speed.setter
+    def speed(self, value):
+        self._speed.value = value
 
     @property
     def color(self):
@@ -36,16 +36,23 @@ class Breathe(Animation):
     def color(self, value):
         self._color.value = value
 
+    def _scale_brightness(self, scale_value, max_steps):
+        return self._color * (scale_value / max_steps).value
+
     async def _breathe(self):
-        # 3 bytes per binary packet, 24 binary packets per pixel, 800 kbps transfer rate
-        delay_time = self.delay * 72 * self.strip.numPixels() / 800000
-        brightness_steps = int(1 / delay_time)
+        # 3 bits per binary value, 24 binary values per pixel (RGB), 800 kbps transfer rate
+        update_delay = 72 * self.strip.numPixels() / 800000
+
+        minimum_steps = int(1 / update_delay)
+
+        # 4.4 for range of 1 to 5 seconds; 50 for range of speed inputs
+        brightness_steps = minimum_steps + 4.4 * (50 - self.speed)
 
         for i in list(range(0, brightness_steps + 1)) + list(reversed(range(0, brightness_steps))):
             for j in range(self.strip.numPixels()):
-                self.strip.setPixelColor(j, (self._color * (i / brightness_steps)).value)
+                self.strip.setPixelColor(j, self._scale_brightness(i, brightness_steps))
             self.strip.show()
-            await asyncio.sleep(delay_time)
+            await asyncio.sleep(update_delay)
 
     @run_decorator
     async def run(self):
@@ -54,7 +61,7 @@ class Breathe(Animation):
 
     def get_settings(self):
         settings = [
-            self._delay,
+            self._speed,
             self._color
         ]
         return super().get_settings() + settings
--- tests/testapp.py
+++ tests/testapp.py
@@ -69,4 +69,5 @@ def handle_animations(animation):
     socketio.emit("settings", lightshow.settings)
 
 
-socketio.run(app)
+if __name__ == '__main__':
+    socketio.run(app)
